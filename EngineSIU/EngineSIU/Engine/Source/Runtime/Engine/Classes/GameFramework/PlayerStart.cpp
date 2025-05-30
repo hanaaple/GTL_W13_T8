1ff9aaf2bce53f65da4232cc8e42f43065b64d04
gudtldn <gudtldn1234@gmail.com>
2025-05-30 16:31:58 +0900

    feat: PlayerStart.cpp/h 파일을 프로젝트에 추가

---
+++ EngineSIU/EngineSIU/Engine/Source/Runtime/Engine/Classes/GameFramework/PlayerStart.cpp
@@ -0,0 +1,13 @@
+﻿#include "PlayerStart.h"
+#include "Components/CapsuleComponent.h"
+#include "InteractiveToolsFramework/BaseGizmos/GizmoArrowComponent.h"
+
+
+APlayerStart::APlayerStart()
+{
+    CapsuleComponent = AddComponent<UCapsuleComponent>(TEXT("CapsuleComponent"));
+    CapsuleComponent->SetHalfHeight(9.2f);
+    CapsuleComponent->SetRadius(4.0f);
+    // CapsuleComponent->SetCollisionEnabled(false);
+    SetRootComponent(CapsuleComponent);
+}
---
+++ EngineSIU/EngineSIU/Engine/Source/Runtime/Engine/Classes/GameFramework/PlayerStart.h
@@ -0,0 +1,31 @@
+﻿#pragma once
+#include "Actor.h"
+
+
+class UCapsuleComponent;
+class UGizmoArrowComponent;
+
+class APlayerStart : public AActor
+{
+    DECLARE_CLASS(APlayerStart, AActor)
+
+public:
+    APlayerStart();
+
+public:
+    UCapsuleComponent* GetCapsuleComponent() const { return CapsuleComponent; }
+
+public:
+    UPROPERTY(
+        UCapsuleComponent*, CapsuleComponent, ;
+    )
+
+    UPROPERTY(
+        EditAnywhere, {},
+        FName, PlayerStartTag, ;
+    )
+
+#if WITH_EDITOR && 0 // TODO: 나중에 ArrowComponent 사용해서 Rotation 표시
+    UGizmoArrowComponent* GizmoArrowComponent;
+#endif
+};
