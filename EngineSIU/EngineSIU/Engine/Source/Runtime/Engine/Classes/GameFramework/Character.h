711841b8bfc84f6cee8b70ccc1e2a47086156225
gudtldn <gudtldn1234@gmail.com>
2025-06-02 21:19:39 +0900

    feat: ACharacter 클래스에 캡슐 및 스켈레탈 메시 컴포넌트 추가

--- EngineSIU/EngineSIU/Engine/Source/Runtime/Engine/Classes/GameFramework/Character.h
+++ EngineSIU/EngineSIU/Engine/Source/Runtime/Engine/Classes/GameFramework/Character.h
@@ -1,12 +1,30 @@
 ﻿#pragma once
 #include "Pawn.h"
 
+class UCapsuleComponent;
+class USkeletalMeshComponent;
+
 
 class ACharacter : public APawn
 {
     DECLARE_CLASS(ACharacter, APawn)
 
 public:
-    ACharacter() = default;
-    
+    ACharacter();
+    virtual ~ACharacter() override = default;
+
+public:
+    UCapsuleComponent* GetCapsuleComponent() const { return CapsuleComponent; }
+    USkeletalMeshComponent* GetMesh() const { return MeshComponent; }
+
+private:
+    UPROPERTY(
+        VisibleAnywhere,
+        UCapsuleComponent*, CapsuleComponent, ;
+    )
+
+    UPROPERTY(
+        VisibleAnywhere,
+        USkeletalMeshComponent*, MeshComponent, ;
+    )
 };
